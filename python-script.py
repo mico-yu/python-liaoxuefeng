f00c3df96f26cd42005c09dc28305f8c2d0e28ba
mico-yu <598376883@qq.com>
2019-02-22 17:32:31 +0800

    2019年2月22日17:32:00

--- python-script.py
+++ python-script.py
@@ -1,22 +1,25 @@
-#coding=utf-8
+
 import math
+my_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 '''调用函数
 r=input("输入半径：")
 r=float(r)
 pi=3.14
 mianji = pi*r*r
 print(mianji)
-
 '''
 '''定义函数'''
+
+
 def my_abs(x):
-	if not isinstance(x,(int, float)):
-		raise TypeError('bad operand type')
-	if x>= 0:
-	    return x
-	else:
-		return -1*x
-#print(my_abs('a'))
+    if not isinstance(x, (int, float)):
+        raise TypeError('bad operand type')
+    if x >= 0:
+        return x
+    else:
+        return -1*x
+# print(my_abs('a'))
+
 
 def mov(x, y, step, angle=0):
         for m in (x, y, step, angle):
@@ -24,104 +27,116 @@ def mov(x, y, step, angle=0):
                         raise TypeError('bad operand type')
         nx = x + step * math.cos(angle)
         ny = y + step * math.sin(angle)
-        return nx,ny
-#r = mov(1,1,2)
-#print(r)
+        return nx, ny
+# r = mov(1,1,2)
+# print(r)
 
-def quadratic(a,b,c):
-        for m in (a,b,c):
-                if not isinstance(m,(int ,float)):
+
+def quadratic(a, b, c):
+        for m in (a, b, c):
+                if not isinstance(m, (int, float)):
                         raise TypeError("bad operand type")
-        if a==0 and b==0 and c==0:
+        if a == 0 and b == 0 and c == 0:
                 print("任意解")
                 return
-        if a ==0 and b==0 :
+        if a == 0 and b == 0:
                 print("参数错误")
                 return
         if a == 0:
                 return 1.0*c/b
-        dd=b*b-4*a*c
-        d=float(dd)
+        dd = b * b - 4 * a * c
+        d = float(dd)
         
-        if d >=0:
+        if d >= 0:
                 d = math.sqrt(d)
                 return (-1*b+d)/(2*a), (-1*b-d)/(2*a)
         else:
                 print("无解")
 
-#r=quadratic(1, 3, -4)
-#print(r)
+# r=quadratic(1, 3, -4)
+# print(r)
 
-def my_power(x,n=2):
+
+def my_power(x, n=2):
         for m in (x, n):
                 if not isinstance(m, (int, float)):
                         raise TypeError('bad operand type')
-        if n==0:
+        if n == 0:
                 return 1
         sign = 0
         if n < 0:
-                sign =1
-                n = -1*n
-        res =1
+                sign = 1
+                n = -1 * n
+        res = 1
         while n >= 1:
                 res = res * x
-                n = n-1
+                n = n - 1
         if sign == 0:
                 return res
         else:
                 return 1/res
 
 
-#r = my_power(0,0)
-#print(r)
+# r = my_power(0,0)
+# print(r)
 
-def add_end(L=None):
-        if L == None:
-                L = []
-        L.append("end")
-        return L
+def add_end(lo=None):
+        if lo == None:
+                lo = []
+        lo.append("end")
+        return lo
+
+
 '''
 L = add_end()
 print(L)
 L= add_end()
 print(L)
 '''
-#可变参数允许你输入N个参数，这些参数在函数调用时自动组装成一个tuple
+
+
+# 可变参数允许你输入N个参数，这些参数在函数调用时自动组装成一个tuple
 def calc(*numbers):
         sum =0
         for n in numbers:
                 sum = sum + n*n
         return sum
 
-#num = [1,2,3]
-#r=calc(*num)
-#print(r)
+# num = [1,2,3]
+# r=calc(*num)
+# print(r)
 
-#关键字参数,允许你传入N个含参数名的参数，这些关键字参数在函数调用时自动组装成一个dict
+
+# 关键字参数,允许你传入N个含参数名的参数，这些关键字参数在函数调用时自动组装成一个dict
 def person(name, age, **kw):
         print("name:",name,"age:",age,"others:",kw)
 
-#person('ysm', 27,city="beijing")
-#extra={'city':'bj','job':'engineer'}
-#person('ysm',27,**extra)
+# person('ysm', 27,city="beijing")
+# extra={'city':'bj','job':'engineer'}
+# person('ysm',27,**extra)
 
-def new_person(name, age, *,city,zipcode):
+
+def new_person(name, age, *, city, zipcode):
         pass
-#new_person('jack',88,city='bj',zipcode=1002)
+# new_person('jack',88,city='bj',zipcode=1002)
 
-#命名关键字参数
-#限定了关键字只有ciy和job
+
+# 命名关键字参数
+# 限定了关键字只有ciy和job
 def new_new_person(name, age, *, city, job):
         pass
 
 
-#如果参数中已经有一个可变参数，后面跟着的命名关键字就不再需要一个特殊分隔符*了
+# 如果参数中已经有一个可变参数，后面跟着的命名关键字就不再需要一个特殊分隔符*了
 def new_new_new_person(name, age, *args, city, job):
         pass
 
-#参数组合
+
+# 参数组合
 def f1(a, b, c=0, *args, **kw):
         print('a=',a,'b=',b,'c=',c,'args=',args, 'kw=',kw)
+
+
 def f2(a, b, c=0, *, d, **kw):
         print('a=',a,'b=',b,'c=',c,'d=',d,'kw=',kw)
 
@@ -186,6 +201,7 @@ def mytrim(s):
 #res = mytrim('  helloworld   ')
 #print(res)
 
+
 def finMinAndMax(L):
     if L==None or L==[]:
          return (None,None)
@@ -199,6 +215,7 @@ def finMinAndMax(L):
     return (min,max) 
 #print(finMinAndMax([7]))
 
+
 def fib(max):
         n,a,b = 0,0,1
         while n<max:
@@ -209,9 +226,126 @@ def fib(max):
 
 #f = fib(6)
 
-for n in fib(6):
-        print(n)
-        
+#for n in fib(6):
+#       print(n)
+f=abs
+def my_add(x,y,f):
+        return f(x)+f(y)
+#res = my_add(-10,-10,f)
+#print(res)
+
+def ff(x):
+        return x*x
+#r=map(ff,[1,2,3,4,5,6,7,8,9])
+#list(r)
+
+#test map and reduce
+from functools import reduce
+def str2float(s):
+    m = len(s) - s.index('.') - 1
+    def char2num(s):
+        digits = {'0':0, '1':1, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8,'9':9, '.':None}
+        return digits[s]
+
+    def fn(x,y):
+        if y == None:
+            return x
+        else:
+            return x*10 + y
+
+    return reduce(fn, map(char2num, s))/(10**m)
+
+def is_odd(n):
+    return n%2 == 1
+#r = tuple( filter(is_odd, my_list))
+def not_empty(s):
+    return s and s.strip()
+r = list(filter(not_empty, ['A'," B", 'C ',' ','']))
+print(r)
+
+#素数生成
+def main():
+    for n in primes():
+        if n < 1000:
+            print(n)
+        else:
+            break;
+def _odd_iter():
+    n=1
+    while True:
+        n=n+2
+        yield n
+def _not_divisible(n):
+    return lambda x:x%n>0
+def primes():
+    yield 2
+    it = _odd_iter()
+    while True:
+        n=next(it)
+        yield n
+        it=filter(_not_divisible(n), it)
+
+#if __name__ == '__main__':
+#main()
+def triangles():
+    L = [1]
+    while True:
+        yield(L)
+        L = [1 if (i == 0 or i == len(L)) else L[i-1] + L[i] for i in range(len(L) + 1)]
+
+
+def lazy_sum(*args):
+    def sum():
+        ax=0
+        for n in args:
+            ax=ax+n
+        return ax
+    return sum
+
+
+def count():
+    fs=[]
+    for i in range(1, 4):
+        def f():
+            return i*i
+        fs.append(f)
+    return fs
+'''
+f1, f2, f3 = count()
+f1()
+f2()
+f3()
+'''
+
+
+def count_pro():
+    def f_(j):
+        def g():
+            return j*j
+        return g
+    fs = []
+    for i in range(1, 4):
+        fs.append(f_(i))
+    return fs
+
+
+# f1, f2, f3 = count_pro()
+
+# '''
+def count_pp():
+    def f(j):
+        return j*j
+    fs = []
+    for i in range(1, 4):
+        fs.append(f(i))
+    return fs
+
+
+f1, f2, f3 = count_pp()
+# '''
+print(f1)
+print(f2)
+print(f3)
 
 
 
@@ -224,4 +358,14 @@ for n in fib(6):
 
 
 
-t=input("pause")
+
+
+
+
+
+
+
+
+
+
+
